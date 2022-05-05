aa587c5d0d440ba4d95348e0e5a98133d428963c
Giovanni Pollo <giovannipollo98@gmail.com>
2022-05-05 19:50:11 +0200

    Added chrome configuration

--- Chrome Extension/lib/focused-element.js
+++ Chrome Extension/lib/focused-element.js
@@ -1,43 +1,37 @@
 (() => {
-
-  function nextField(){
-    var inputs = document.getElementsByTagName('input');
-        for (var i = 0; i < inputs.length; i++) {
-        if (document.activeElement.id == inputs[i].id && i+1 < inputs.length ) {
-          return inputs[i+1];
-        }
+  function nextField() {
+    var inputs = document.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {
+      if (document.activeElement.id == inputs[i].id && i + 1 < inputs.length) {
+        return inputs[i + 1];
       }
-      return undefined;
-	}
+    }
+    return undefined;
+  }
 
-	function getPassword(hostname){
-		const Http = new XMLHttpRequest();
-		const url='https://jsonplaceholder.typicode.com/posts';
-		Http.open("GET", url);
-		Http.send();
-
-		Http.onreadystatechange = (e) => {
-		  console.log(Http.responseText)
-		}
-	}
   /** @type {Element | null} */
   let lastFocusedElement = null;
 
   const logId = setInterval(() => {
     const focusedElement = document.activeElement;
     if (focusedElement !== lastFocusedElement) {
-      if (focusedElement.tagName.toLowerCase() == "input"){
-        if(focusedElement.type.toLowerCase() != "password"){
+      if (focusedElement.tagName.toLowerCase() == "input") {
+        if (focusedElement.type.toLowerCase() != "password") {
           let nextInput = nextField();
-          if (nextInput.type.toLowerCase() == "password"){
-            /* Username + Password fields */ 
-            console.log("Found username and password fields for: " + window.location.hostname);
+          if (nextInput.type.toLowerCase() == "password") {
+            /* Username + Password fields */
+            console.log(
+              "Found username and password fields for: " +
+                window.location.hostname
+            );
             focusedElement.value = "username";
             nextInput.value = "password";
           }
         } else {
           /* Only password field */
-          console.log("Found only username field for: " + window.location.hostname);
+          console.log(
+            "Found only username field for: " + window.location.hostname
+          );
           focusedElement.value = "password";
         }
       }
