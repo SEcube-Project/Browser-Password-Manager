a6f19b358363155483af994ff3f260dba16da353
Giovanni Pollo <giovannipollo98@gmail.com>
2022-05-08 13:24:50 +0200

    Added a button to test material UI

--- Chrome Extension/src/popup/popup.tsx
+++ Chrome Extension/src/popup/popup.tsx
@@ -2,6 +2,17 @@ import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import { fetchApi } from "../utils/api";
 import "./popup.css";
+
+import Box from '@mui/material/Box';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import PrintIcon from '@mui/icons-material/Print';
+import ShareIcon from '@mui/icons-material/Share';
+
+
 // variable to test the code without using real API but only JSON file
 const DEBUG=true;
 
@@ -15,8 +26,39 @@ const App: React.FC<{}> = () => {
         console.log(err);
       });
   }, []);
-  return <div> Hello world from extension</div>;
+  return BasicSpeedDial();
 };
+
+
+const actions = [
+  { icon: <FileCopyIcon />, name: 'Copy' },
+  { icon: <SaveIcon />, name: 'Save' },
+  { icon: <PrintIcon />, name: 'Print' },
+  { icon: <ShareIcon />, name: 'Share' },
+];
+
+
+export default function BasicSpeedDial() {
+  return (
+    <Box sx={{ height: 500, width: 500, transform: 'translateZ(0px)', flexGrow: 1 }}>
+      <SpeedDial
+        ariaLabel="SpeedDial basic example"
+        sx={{ position: 'absolute', bottom: 1, right: 1, top: 1, left: 420 }}
+        icon={<SpeedDialIcon />}
+      >
+        {actions.map((action) => (
+          <SpeedDialAction onClick={() => {
+            console.log("clicked");
+          }}
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+          />
+        ))}
+      </SpeedDial>
+    </Box>
+  );
+}
 
 
 const root = document.createElement("div");
