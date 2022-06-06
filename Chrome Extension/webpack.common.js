e91b05f01086a9d0a1468df144cef4cda576204a
Giovanni Pollo <giovannipollo98@gmail.com>
2022-06-06 11:23:05 +0200

    Added obfuscator for js files

--- Chrome Extension/webpack.common.js
+++ Chrome Extension/webpack.common.js
@@ -1,69 +1,111 @@
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
-const HtmlPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+var WebpackObfuscator = require("webpack-obfuscator");
 
+plugins: [
+  new WebpackObfuscator(
+    {
+      rotateStringArray: true,
+    },
+    ["excluded_bundle_name.js"]
+  ),
+];
 module.exports = {
   entry: {
-    popup: path.resolve('src/popup/popup.tsx'),
-    options: path.resolve('src/options/options.tsx'),
-    background: path.resolve('src/background/background.ts'),
-    content: path.resolve('src/content/content.ts'),
+    popup: path.resolve("src/popup/popup.tsx"),
+    options: path.resolve("src/options/options.tsx"),
+    background: path.resolve("src/background/background.ts"),
+    content: path.resolve("src/content/content.ts"),
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
-        type: 'asset/resource'
-      }
-    ]
+        type: "asset/resource",
+      },
+    ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
   plugins: [
     new CleanWebpackPlugin({
       cleanStaleWebpackAssets: false,
     }),
+    new WebpackObfuscator(
+      {
+        compact: true,
+        controlFlowFlattening: true,
+        controlFlowFlatteningThreshold: 1,
+        deadCodeInjection: true,
+        deadCodeInjectionThreshold: 1,
+        debugProtection: true,
+        debugProtectionInterval: 4000,
+        disableConsoleOutput: true,
+        identifierNamesGenerator: 'hexadecimal',
+        log: false,
+        numbersToExpressions: true,
+        renameGlobals: false,
+        selfDefending: true,
+        simplify: true,
+        splitStrings: true,
+        splitStringsChunkLength: 5,
+        stringArray: true,
+        stringArrayCallsTransform: true,
+        stringArrayEncoding: ['rc4'],
+        stringArrayIndexShift: true,
+        stringArrayRotate: true,
+        stringArrayShuffle: true,
+        stringArrayWrappersCount: 5,
+        stringArrayWrappersChainedCalls: true,    
+        stringArrayWrappersParametersMaxCount: 5,
+        stringArrayWrappersType: 'function',
+        stringArrayThreshold: 1,
+        transformObjectKeys: true,
+        unicodeEscapeSequence: false    
+      }["background.js, content.js, popup.js, options.js, vendors-node_modules_mui_icons-material_AccountCircle_js-node_modules_mui_icons-material_AddC-c98725, vendors-node_modules_mui_material_Box_Box_js-node_modules_mui_material_Button_Button_js-node_-2a77c8, vendors-node_modules_mui_material_Card_Card_js-node_modules_mui_material_CardContent_CardCont-a0946a"],
+    ),
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve('src/static'),
-          to: path.resolve('dist'),
-        }
-      ]
+          from: path.resolve("src/static"),
+          to: path.resolve("dist"),
+        },
+      ],
     }),
-    ...getHtmlPlugins([
-      'popup',
-      'options'
-    ]),
+    ...getHtmlPlugins(["popup", "options"]),
   ],
   output: {
-    filename: '[name].js',
-    path: path.resolve('dist'),
+    filename: "[name].js",
+    path: path.resolve("dist"),
   },
   optimization: {
     splitChunks: {
       chunks(chunk) {
-        return chunk.name !== 'contentScript' && chunk.name !== 'background'
-      }
+        return chunk.name !== "contentScript" && chunk.name !== "background";
+      },
     },
-  }
-}
+  },
+};
 
 function getHtmlPlugins(chunks) {
-  return chunks.map(chunk => new HtmlPlugin({
-    title: 'React Extension',
-    filename: `${chunk}.html`,
-    chunks: [chunk],
-  }))
+  return chunks.map(
+    (chunk) =>
+      new HtmlPlugin({
+        title: "React Extension",
+        filename: `${chunk}.html`,
+        chunks: [chunk],
+      })
+  );
 }
