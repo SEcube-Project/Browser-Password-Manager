9614d9b7a50e03e2412d40226be9497dd385af7e
Matteo Battilana <matteo@matteobattilana.com>
2022-06-18 17:18:25 +0200

    Added setup tutorial for firmware, added missing free in C firmware code, added Release as default compile version

--- Host Password Manager/Debug/examples/manual_pass_management.d
+++ Host Password Manager/Debug/examples/manual_pass_management.d
@@ -1,72 +1,72 @@
 examples/manual_pass_management.o: ../examples/manual_pass_management.cpp \
  ../examples/../sources/L1/L1.h ../examples/../sources/L1/../L0/L0.h \
- ../examples/../sources/L1/../L0/Commodities\ API/commodities_api.h \
- ../examples/../sources/L1/../L0/Commodities\ API/../L0\ Base/L0_base.h \
- ../examples/../sources/L1/../L0/Commodities\ API/../L0\ Base/../L0_enumerations.h \
- ../examples/../sources/L1/../L0/Communication\ API/communication_api.h \
- ../examples/../sources/L1/../L0/Communication\ API/../L0\ Base/L0_base.h \
- ../examples/../sources/L1/../L0/Provision\ API/provision_api.h \
- ../examples/../sources/L1/../L0/L0\ Base/L0_base.h \
- ../examples/../sources/L1/L1\ Base/L1_base.h \
- ../examples/../sources/L1/L1\ Base/../../L0/L0\ Base/L0_base.h \
- ../examples/../sources/L1/L1\ Base/../Crypto\ Libraries/aes256.h \
- ../examples/../sources/L1/L1\ Base/../../L0/L0_error_manager.h \
- ../examples/../sources/L1/L1\ Base/../Crypto\ Libraries/pbkdf2.h \
- ../examples/../sources/L1/L1\ Base/../Crypto\ Libraries/sha256.h \
- ../examples/../sources/L1/L1\ Base/../L1_error_manager.h \
- ../examples/../sources/L1/L1\ Base/../L1_enumerations.h \
- ../examples/../sources/L1/L1\ Base/../../L0/L0_enumerations.h \
- ../examples/../sources/L1/Login-Logout\ API/login_logout_api.h \
- ../examples/../sources/L1/Login-Logout\ API/../L1\ Base/L1_base.h \
- ../examples/../sources/L1/Security\ API/security_api.h \
- ../examples/../sources/L1/Security\ API/../L1\ Base/L1_base.h \
- ../examples/../sources/L1/Utility\ API/utility_api.h \
- ../examples/../sources/L1/Utility\ API/../L1\ Base/L1_base.h
+ ../examples/../sources/L1/../L0/Commodities_API/commodities_api.h \
+ ../examples/../sources/L1/../L0/Commodities_API/../L0_Base/L0_base.h \
+ ../examples/../sources/L1/../L0/Commodities_API/../L0_Base/../L0_enumerations.h \
+ ../examples/../sources/L1/../L0/Communication_API/communication_api.h \
+ ../examples/../sources/L1/../L0/Communication_API/../L0_Base/L0_base.h \
+ ../examples/../sources/L1/../L0/Provision_API/provision_api.h \
+ ../examples/../sources/L1/../L0/L0_Base/L0_base.h \
+ ../examples/../sources/L1/L1_Base/L1_base.h \
+ ../examples/../sources/L1/L1_Base/../../L0/L0_Base/L0_base.h \
+ ../examples/../sources/L1/L1_Base/../Crypto_Libraries/aes256.h \
+ ../examples/../sources/L1/L1_Base/../../L0/L0_error_manager.h \
+ ../examples/../sources/L1/L1_Base/../Crypto_Libraries/pbkdf2.h \
+ ../examples/../sources/L1/L1_Base/../Crypto_Libraries/sha256.h \
+ ../examples/../sources/L1/L1_Base/../L1_error_manager.h \
+ ../examples/../sources/L1/L1_Base/../L1_enumerations.h \
+ ../examples/../sources/L1/L1_Base/../../L0/L0_enumerations.h \
+ ../examples/../sources/L1/Login-Logout_API/login_logout_api.h \
+ ../examples/../sources/L1/Login-Logout_API/../L1_Base/L1_base.h \
+ ../examples/../sources/L1/Security_API/security_api.h \
+ ../examples/../sources/L1/Security_API/../L1_Base/L1_base.h \
+ ../examples/../sources/L1/Utility_API/utility_api.h \
+ ../examples/../sources/L1/Utility_API/../L1_Base/L1_base.h
 
 ../examples/../sources/L1/L1.h:
 
 ../examples/../sources/L1/../L0/L0.h:
 
-../examples/../sources/L1/../L0/Commodities\ API/commodities_api.h:
+../examples/../sources/L1/../L0/Commodities_API/commodities_api.h:
 
-../examples/../sources/L1/../L0/Commodities\ API/../L0\ Base/L0_base.h:
+../examples/../sources/L1/../L0/Commodities_API/../L0_Base/L0_base.h:
 
-../examples/../sources/L1/../L0/Commodities\ API/../L0\ Base/../L0_enumerations.h:
+../examples/../sources/L1/../L0/Commodities_API/../L0_Base/../L0_enumerations.h:
 
-../examples/../sources/L1/../L0/Communication\ API/communication_api.h:
+../examples/../sources/L1/../L0/Communication_API/communication_api.h:
 
-../examples/../sources/L1/../L0/Communication\ API/../L0\ Base/L0_base.h:
+../examples/../sources/L1/../L0/Communication_API/../L0_Base/L0_base.h:
 
-../examples/../sources/L1/../L0/Provision\ API/provision_api.h:
+../examples/../sources/L1/../L0/Provision_API/provision_api.h:
 
-../examples/../sources/L1/../L0/L0\ Base/L0_base.h:
+../examples/../sources/L1/../L0/L0_Base/L0_base.h:
 
-../examples/../sources/L1/L1\ Base/L1_base.h:
+../examples/../sources/L1/L1_Base/L1_base.h:
 
-../examples/../sources/L1/L1\ Base/../../L0/L0\ Base/L0_base.h:
+../examples/../sources/L1/L1_Base/../../L0/L0_Base/L0_base.h:
 
-../examples/../sources/L1/L1\ Base/../Crypto\ Libraries/aes256.h:
+../examples/../sources/L1/L1_Base/../Crypto_Libraries/aes256.h:
 
-../examples/../sources/L1/L1\ Base/../../L0/L0_error_manager.h:
+../examples/../sources/L1/L1_Base/../../L0/L0_error_manager.h:
 
-../examples/../sources/L1/L1\ Base/../Crypto\ Libraries/pbkdf2.h:
+../examples/../sources/L1/L1_Base/../Crypto_Libraries/pbkdf2.h:
 
-../examples/../sources/L1/L1\ Base/../Crypto\ Libraries/sha256.h:
+../examples/../sources/L1/L1_Base/../Crypto_Libraries/sha256.h:
 
-../examples/../sources/L1/L1\ Base/../L1_error_manager.h:
+../examples/../sources/L1/L1_Base/../L1_error_manager.h:
 
-../examples/../sources/L1/L1\ Base/../L1_enumerations.h:
+../examples/../sources/L1/L1_Base/../L1_enumerations.h:
 
-../examples/../sources/L1/L1\ Base/../../L0/L0_enumerations.h:
+../examples/../sources/L1/L1_Base/../../L0/L0_enumerations.h:
 
-../examples/../sources/L1/Login-Logout\ API/login_logout_api.h:
+../examples/../sources/L1/Login-Logout_API/login_logout_api.h:
 
-../examples/../sources/L1/Login-Logout\ API/../L1\ Base/L1_base.h:
+../examples/../sources/L1/Login-Logout_API/../L1_Base/L1_base.h:
 
-../examples/../sources/L1/Security\ API/security_api.h:
+../examples/../sources/L1/Security_API/security_api.h:
 
-../examples/../sources/L1/Security\ API/../L1\ Base/L1_base.h:
+../examples/../sources/L1/Security_API/../L1_Base/L1_base.h:
 
-../examples/../sources/L1/Utility\ API/utility_api.h:
+../examples/../sources/L1/Utility_API/utility_api.h:
 
-../examples/../sources/L1/Utility\ API/../L1\ Base/L1_base.h:
+../examples/../sources/L1/Utility_API/../L1_Base/L1_base.h:
